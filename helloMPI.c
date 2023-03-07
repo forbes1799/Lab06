8b9fb0b0f16ab9a4bd8f7c3d110ede57565e38f6
forbes1799 <123934016+forbes1799@users.noreply.github.com>
2023-03-07 10:03:43 +0000

    Update helloMPI.c

--- helloMPI.c
+++ helloMPI.c
@@ -4,7 +4,7 @@ int main(void) {
 	int myRank;
 	MPI_Init(NULL, NULL);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-	printf("Hi from rank %d\n", myID);
+	printf("Hi from rank %d\n", myRank);
 	MPI_Finalize();
 }
 
